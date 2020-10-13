70da8bcd679ddcd7311c043686cd0a54e7641770
egoo0202 <bill.chen.android@gmail.com>
2020-10-13 11:50:53 +0800

    "newnew"

---
+++ AAART/src/main/java/_35_csrService/controller/FoundingKids.java
@@ -0,0 +1,42 @@
+package _35_csrService.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/_35_csrService/FoundingKids")
+public class FoundingKids extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FoundingKids() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF8");
+		response.setContentType("text/html;charset=UTF-8");
+		String s = request.getParameter("kidsLocation");
+		System.out.println(s);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
